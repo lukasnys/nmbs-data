c69d60f7409791de5f958e2dbf8f43abec554f9e
Lukas Nys <lukas.nys@ugent.be>
2022-03-24 23:30:08 +0100

    fix port

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ app.get("/train", (req, res, next) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
