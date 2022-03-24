fb14180f85dcc233a502c974bc64a9f9430d4814
Lukas Nys <lukas.nys@ugent.be>
2022-03-24 23:25:37 +0100

    make it better

---
+++ index.js
@@ -0,0 +1,30 @@
+var GtfsRealtimeBindings = require("gtfs-realtime-bindings");
+var request = require("request");
+var express = require("express");
+app = express();
+
+app.get("/train", (req, res, next) => {
+    var requestSettings = {
+        method: "GET",
+        url: "https://sncb-opendata.hafas.de/gtfs/realtime/c21ac6758dd25af84cca5b707f3cb3de",
+        encoding: null,
+    };
+
+    request(requestSettings, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
+            const feedJSON = feed.entity.map(function (entity) {
+                if (entity.tripUpdate) {
+                    return entity.tripUpdate;
+                }
+            });
+            res.json(feedJSON);
+        } else {
+            res.error("something went wrong");
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
